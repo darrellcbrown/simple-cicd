1a220f3ac546dfc537742f375901c69f4307e81e
darrellcbrown <darrellcbrown@yahoo.com>
2022-01-24 22:57:41 -0600

    updating files

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,9 +11,20 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('darrellcbrown/simple-cicd', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "307158967668", region: "us-west-2" }
+    }));
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, 'testing', {
+      env: { account: '307158967668', region: 'us-west-2' }
+    })); 
+    
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
   }
 }
